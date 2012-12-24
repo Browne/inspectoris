0f108f5832ad0b388a79be455d58f2d72159bf1d
Eliot Brown <eliotjbrown@gmail.com>
2012-12-24 02:21:21 +0000

    Script updates

---
+++ scripts/config.js
@@ -0,0 +1,20 @@
+window.slidesConfig = window.slidesConfig || {
+  // Slide settings
+  settings : {
+    useBuilds: true,
+    useGDDBranding: true
+    //hashtag: '#html5'
+  },
+  info: {
+    // Personal info
+    name: 'Eliot Brown',
+    pic: 'https://secure.gravatar.com/avatar/78d8a2a03bea7cd74642c26038df89c9?s=420&d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png',
+    gplus: 'http://goo.gl/pws4Q',
+
+    // GDD info
+    location: 'Reading, England',
+    country: 'EN',
+    date: new Date('December 24 2012'),
+    logo: 'http://www.letterworks.co.uk/images/content/creativity.jpg'
+  }
+};
